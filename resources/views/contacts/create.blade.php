79739e968364136786c883899887e8795cfd1e5e
bhn0108 <bhn0108@icloud.com>
2023-06-05 21:27:58 +0900

    mod html

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -33,7 +33,7 @@
                                             <div class="relative">
                                                 <label for="email"
                                                     class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                                <input type="text" id="email" name="email"
+                                                <input type="email" id="email" name="email"
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                             </div>
                                         </div>
@@ -41,15 +41,15 @@
                                             <div class="relative">
                                                 <label for="url"
                                                     class="leading-7 text-sm text-gray-600">ホームページ</label>
-                                                <input type="text" id="url" name="url"
+                                                <input type="url" id="url" name="url"
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                             </div>
                                         </div>
                                         <div class="p-2 w-full">
                                             <div class="relative">
                                                 <label class="leading-7 text-sm text-gray-600">性別</label><br>
-                                                <input type="radio" id="gender" value="0">男性
-                                                <input type="radio" id="gender" value="1">女性
+                                                <input type="radio" name="gender" value="0">男性
+                                                <input type="radio" name="gender" value="1">女性
                                             </div>
                                         </div>
                                         <div class="p-2 w-full">
